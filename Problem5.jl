c5ee341ae945a6cfdeaec603c9f4e8811bbf46d3
Priyankabedekar <cs20m050@smail.iitm.ac.in>
2021-03-06 14:27:47 +0530

    2

--- Problem5.jl
+++
@@ -1,8 +0,0 @@
-#Theoretically
-i = 0
-Probability = 1
-while Probability > 0.001
-    global Probability = Probability - (binomial(8,i) * (1/78)^i * (77/78)^(8-i))
-    global i += 1
-end
-println("The probability of password getting stored is less than or equal to 0.001 when at least " , i , " characters are same.")
